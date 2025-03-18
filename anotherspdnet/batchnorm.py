a039b2145be87ae8c031d754ef0ffec9fcfaf41f
Ammar Mian <ammar.mian@protonmail.com>
2025-03-18 18:30:52 +0100

    Added version requirement for scipy

--- anotherspdnet/batchnorm.py
+++ anotherspdnet/batchnorm.py
@@ -5,10 +5,12 @@ from typing import List, Optional, Tuple
 import torch
 from torch import nn
 from .functions import (
-        SqrtmEigFunction, InvSqrtmEigFunction, 
-        LogEigFunction, ExpEigFunction,
-        spd_affine_invariant_geodesic
-    )
+    SqrtmEigFunction,
+    InvSqrtmEigFunction,
+    LogEigFunction,
+    ExpEigFunction,
+    spd_affine_invariant_geodesic,
+)
 
 
 from geoopt.manifolds import SymmetricPositiveDefinite
@@ -18,12 +20,17 @@ import warnings
 
 # from pyriemann.utils.mean import mean_riemann
 
+
 # Riemannian mean
 # TODO : compute the mean along axes we want
 # ---------------
-def riemannian_mean_spd(X: torch.Tensor, initial_stepsize: float = 1.0, 
-                     max_iter: int = 5, tol: float = 1e-6, 
-                     weights: Optional[torch.Tensor] = None) -> torch.Tensor:
+def riemannian_mean_spd(
+    X: torch.Tensor,
+    initial_stepsize: float = 1.0,
+    max_iter: int = 5,
+    tol: float = 1e-6,
+    weights: Optional[torch.Tensor] = None,
+) -> torch.Tensor:
     """Riemannian mean for SPD matrices.
 
     Inspired by pyRiemann's implementation.
@@ -56,54 +63,51 @@ def riemannian_mean_spd(X: torch.Tensor, initial_stepsize: float = 1.0,
     if weights is None:
         weights = torch.ones(X.shape[:-2], dtype=X.dtype, device=X.device)
     else:
-        assert weights.shape == X.shape[:len(X.shape) - 2]
+        assert weights.shape == X.shape[: len(X.shape) - 2]
         assert torch.all(weights >= 0), "Weights must be positive"
     weights = weights / torch.sum(weights)
 
     with torch.no_grad():
-        
         # Initialisation
-        mean = torch.einsum('...ij,...->ij', X, weights)
+        mean = torch.einsum("...ij,...->ij", X, weights)
         nu = initial_stepsize
         tau = torch.finfo(X.dtype).max
         crit = torch.finfo(X.dtype).max
 
         # Loop
         for _ in range(max_iter):
-
             # Compute useful matrices in tangent space
             eigvals, eigvecs = torch.linalg.eigh(mean)
             sqrt_eigvals = torch.sqrt(eigvals)
-            inv_sqrt_eigvals = 1/sqrt_eigvals
-            C12 = eigvecs @ torch.diag_embed(sqrt_eigvals) @ \
-                    eigvecs.transpose(-2, -1)
-            C12_inv = eigvecs @ torch.diag(inv_sqrt_eigvals) @ \
-                    eigvecs.transpose(-2, -1)
+            inv_sqrt_eigvals = 1 / sqrt_eigvals
+            C12 = eigvecs @ torch.diag_embed(sqrt_eigvals) @ eigvecs.transpose(-2, -1)
+            C12_inv = eigvecs @ torch.diag(inv_sqrt_eigvals) @ eigvecs.transpose(-2, -1)
 
             # Getting the tangent space matrices
             eigvals, eigvecs = torch.linalg.eigh(C12_inv @ X @ C12_inv)
             log_eigvals = torch.log(eigvals)
-            log_matrices = torch.einsum('...ij,...jk,...kl->...il',
-                                        eigvecs,
-                                        torch.diag_embed(log_eigvals),
-                                        eigvecs.transpose(-2, -1))
-            J = torch.einsum('...,...ij->ij', weights, log_matrices)
+            log_matrices = torch.einsum(
+                "...ij,...jk,...kl->...il",
+                eigvecs,
+                torch.diag_embed(log_eigvals),
+                eigvecs.transpose(-2, -1),
+            )
+            J = torch.einsum("...,...ij->ij", weights, log_matrices)
 
             # Getting the new mean on the manifold
-            eigvals, eigvecs = torch.linalg.eigh(nu*J)
+            eigvals, eigvecs = torch.linalg.eigh(nu * J)
             exp_eigvals = torch.exp(eigvals)
-            exp_J = eigvecs @ torch.diag(exp_eigvals) @ \
-                    eigvecs.transpose(-2, -1)
+            exp_J = eigvecs @ torch.diag(exp_eigvals) @ eigvecs.transpose(-2, -1)
             mean = C12 @ exp_J @ C12
 
             # Check for convergence
-            crit = torch.linalg.norm(J, ord='fro')
-            h = nu*crit
+            crit = torch.linalg.norm(J, ord="fro")
+            h = nu * crit
             if h < tau:
-                nu = 0.95*nu
+                nu = 0.95 * nu
                 tau = h
             else:
-                nu = 0.5*nu
+                nu = 0.5 * nu
 
             if crit <= tol or nu <= tol:
                 break
@@ -111,19 +115,22 @@ def riemannian_mean_spd(X: torch.Tensor, initial_stepsize: float = 1.0,
         else:
             warnings.warn(f"Mean did not converge after {max_iter} iterations.")
 
-
     return mean
 
 
 # Batch normalization
 # ------------------
 class BatchNormSPD(nn.Module):
-
-    def __init__(self, n_features: int, momentum: float = 0.1,
-                 tol_mean: float = 1e-6, max_iter_mean: int = 5,
-                 initial_stepsize_mean: float = 1.0, 
-                 device: torch.device = torch.device('cpu'),
-                 dtype: torch.dtype = torch.float64) -> None:
+    def __init__(
+        self,
+        n_features: int,
+        momentum: float = 0.1,
+        tol_mean: float = 1e-6,
+        max_iter_mean: int = 5,
+        initial_stepsize_mean: float = 1.0,
+        device: torch.device = torch.device("cpu"),
+        dtype: torch.dtype = torch.float64,
+    ) -> None:
         """Batch normalization for SPD matrices. From paper:
         Brooks et al., “Riemannian Batch Normalization for SPD Neural Networks.”
         NEURIPS 2019.
@@ -160,8 +167,10 @@ class BatchNormSPD(nn.Module):
         self.dtype = dtype
         self.running_mean = torch.eye(n_features, dtype=dtype, device=device)
         self.bias = ManifoldParameter(
-                torch.eye(n_features, dtype=dtype, device=device),
-                manifold=self.manifold, requires_grad=True)
+            torch.eye(n_features, dtype=dtype, device=device),
+            manifold=self.manifold,
+            requires_grad=True,
+        )
 
     def forward(self, X: torch.Tensor):
         """Forward pass.
@@ -180,34 +189,39 @@ class BatchNormSPD(nn.Module):
         if self.training:
             # Compute the mean of the batch
             new_mean = riemannian_mean_spd(
-                    X, initial_stepsize=self.initial_stepsize_mean,
-                    max_iter=self.max_iter_mean, tol=self.tol_mean)
+                X,
+                initial_stepsize=self.initial_stepsize_mean,
+                max_iter=self.max_iter_mean,
+                tol=self.tol_mean,
+            )
 
             # TODO: Understand why we need to no grad here
             with torch.no_grad():
                 self.running_mean = spd_affine_invariant_geodesic(
-                        self.running_mean, new_mean, self.momentum)
-
+                    self.running_mean, new_mean, self.momentum
+                )
 
         else:
             new_mean = self.running_mean
 
         isqrtm_new_mean = InvSqrtmEigFunction.apply(new_mean)
         sqrtm_bias = SqrtmEigFunction.apply(self.bias)
-        X_centered = torch.einsum('ij,...jk,kl->...il',
-                                isqrtm_new_mean, X, isqrtm_new_mean)
-        X_normalized = torch.einsum('ij,...jk,kl->...il',
-                                sqrtm_bias, X_centered, sqrtm_bias)
+        X_centered = torch.einsum(
+            "ij,...jk,kl->...il", isqrtm_new_mean, X, isqrtm_new_mean
+        )
+        X_normalized = torch.einsum(
+            "ij,...jk,kl->...il", sqrtm_bias, X_centered, sqrtm_bias
+        )
 
         return X_normalized
 
-
     def __repr__(self):
-        return f"BatchNormSPD({self.n_features}, momentum={self.momentum}, "\
-                f"tol_mean={self.tol_mean}, max_iter_mean={self.max_iter_mean}, "\
-                f"initial_stepsize_mean={self.initial_stepsize_mean}, "\
-                f"device={self.device}, dtype={self.dtype})"
+        return (
+            f"BatchNormSPD({self.n_features}, momentum={self.momentum}, "
+            f"tol_mean={self.tol_mean}, max_iter_mean={self.max_iter_mean}, "
+            f"initial_stepsize_mean={self.initial_stepsize_mean}, "
+            f"device={self.device}, dtype={self.dtype})"
+        )
 
     def __str__(self):
         return self.__repr__()
-
--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='anotherspdnet',
-    author='Ammar Mian, Florent Bouchard, Guillaume Ginolhac, Paul Chauchat',
+    name="anotherspdnet",
+    author="Ammar Mian, Florent Bouchard, Guillaume Ginolhac, Paul Chauchat",
     author_email="ammar.mian@univ-smb.fr",
-    version='0.1.0',
-    packages=find_packages(include=['anotherspdnet', 'anotherspdnet.*']),
-    python_requires='>=3.6',
-    install_requires=[
-        'torch>=2.0.1',
-        'geoopt>=0.5.0',
-        'tqdm'
-    ],
-    tests_require=[
-        'pytest',
-        'pytest-cov',
-        'pytest-sugar'
-    ]
+    version="0.1.0",
+    packages=find_packages(include=["anotherspdnet", "anotherspdnet.*"]),
+    python_requires=">=3.6",
+    install_requires=["torch>=2.0.1", "scipy==1.13.0geoopt>=0.5.0", "tqdm"],
+    tests_require=["pytest", "pytest-cov", "pytest-sugar"],
 )
